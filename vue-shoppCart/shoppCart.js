2a4eb3689293131223bd7377b5870cb516ef4010
836151520@qq.com <836151520@qq.com>
2018-10-09 21:29:55 +0800

    循环出数据

---
+++ vue-shoppCart/shoppCart.js
@@ -0,0 +1,42 @@
+var app = new Vue({
+  el:"#shoppCart",
+  data:{
+     listItems:[
+       {
+         shopp:"车工官方旗舰店",
+         goods:[
+           {
+             goods_txt:"嘉实多(Castrol)机油润滑油 极护磁护金嘉护银嘉护 磁护全合成 4L 5W-40 SN",
+              price:"￥"+"229.00"
+           },
+           {
+             goods_txt:"嘉实多(Castrol)机油润滑油 极护磁护金嘉护银嘉护 磁护全合成 4L 5W-40 SN",
+              price:"￥"+"229.00"
+           },
+           {
+             goods_txt:"嘉实多(Castrol)机油润滑油 极护磁护金嘉护银嘉护 磁护全合成 4L 5W-40 SN",
+              price:"￥"+"229.00"
+           }
+
+         ]
+       },
+       {
+         shopp:"京东自营店",
+         goods:[
+           {
+             goods_txt:"嘉实多(Castrol)机油润滑油 极护磁护金嘉护银嘉护 磁护全合成 4L 5W-40 SN",
+              price:"￥"+"229.00"
+           },
+           {
+             goods_txt:"嘉实多(Castrol)机油润滑油 极护磁护金嘉护银嘉护 磁护全合成 4L 5W-40 SN",
+              price:"￥"+"229.00"
+           },
+           {
+             goods_txt:"嘉实多(Castrol)机油润滑油 极护磁护金嘉护银嘉护 磁护全合成 4L 5W-40 SN",
+              price:"￥"+"229.00"
+           },
+         ]
+       },
+     ]
+  }
+})
